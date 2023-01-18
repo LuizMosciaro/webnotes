9205db483f95dd39b1a1c1b771ff7f70cdfef1bf
luizmosciaro <edu.mosciaro@live.com>
2023-01-17 20:02:28 -0400

    fix err 2

--- application.py
+++
@@ -1,9 +0,0 @@
-from website import create_app
-import os
-
-app = application = create_app()
-
-if __name__=='__main__':
-    app.run(debug=True, port=os.getenv("PORT", default=5000))
-
-    
---
+++ main.py
@@ -0,0 +1,9 @@
+from website import create_app
+import os
+
+application = create_app()
+
+if __name__=='__main__':
+    application.run(debug=True, port=os.getenv("PORT", default=5000))
+
+    
