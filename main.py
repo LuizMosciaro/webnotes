e3b380f9c156369503de9c20bf0bee05658735f7
luizmosciaro <edu.mosciaro@live.com>
2023-01-18 17:58:03 -0400

    test 1

--- main.py
+++ main.py
@@ -4,6 +4,6 @@ import os
 application = create_app()
 
 if __name__=='__main__':
-    application.run(debug=True, port=os.getenv("PORT", default=5000))
+    application.run(debug=True, port=os.getenv("PORT", default=5000),host="0.0.0.0")
 
     
